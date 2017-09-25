0204702ca6c28477151cca34c97de5d73ef029e4
weida <weida@weidadeMac-mini.local>
2017-09-25 17:05:17 +0800

    Initial Commit

---
+++ HaiTao/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HaiTao
+//
+//  Created by weida on 2017/9/25.
+//  Copyright © 2017年 JieMao Technology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HaiTao/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HaiTao
+//
+//  Created by weida on 2017/9/25.
+//  Copyright © 2017年 JieMao Technology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
